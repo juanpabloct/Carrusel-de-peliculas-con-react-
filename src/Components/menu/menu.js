b04b60affd47e87aae77495386b0dcb8d2242984
Juan Pablo Castellanos Torres <juanptorres2003@gmail.com>
2022-02-08 23:05:05 -0500

    Se ha añadido en el menu el input de Busqueda

--- src/Components/menu/menu.js
+++ src/Components/menu/menu.js
@@ -1,16 +1,24 @@
 import "./menu.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 export default function Menu(props) {
-  const menu = [
-    { nombre: "Inicio", url: "/" },
-    { nombre: "Series", url: "/series" },
-    { nombre: "Peliculas", url: "/peliculas" },
-  ];
+  const menu = [{ nombre: "Inicio", url: "/" }];
+  const [entrada, setEntrada] = useState("");
+  console.log(entrada);
   return (
     <div className="c-menu">
       {menu.map((value, i) => (
         <div key={i} className="menu">
           {<Link to={value.url}>{value.nombre}</Link>}
+          <div id="buscador">
+            <form onSubmit={(e) => e.preventDefault()}>
+              <input
+                type="search"
+                onChange={(e) => setEntrada(e.target.value)}
+              />
+              <input type="submit" />
+            </form>
+          </div>
         </div>
       ))}
     </div>
