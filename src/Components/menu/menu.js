91124a3b30ba6a3f75254acecd3efdf4c7cd8720
Juan Pablo Castellanos Torres <juanptorres2003@gmail.com>
2022-02-20 09:56:09 -0500

    Componente que contiene el menu

--- src/Components/menu/menu.js
+++ src/Components/menu/menu.js
@@ -1,26 +1,35 @@
-import "./menu.css";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-export default function Menu(props) {
+
+import "./menu.css";
+
+export default function Menu({ onChange }) {
   const menu = [{ nombre: "Inicio", url: "/" }];
   const [entrada, setEntrada] = useState("");
-  console.log(entrada);
+  const buscador = (
+    <div id="buscador">
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input type="search" onChange={(e) => setEntrada(e.target.value)} />
+        <button
+          type="submit"
+          onClick={() => {
+            onChange((value) => entrada);
+          }}
+        >
+          Buscar
+        </button>
+      </form>
+    </div>
+  );
   return (
     <div className="c-menu">
       {menu.map((value, i) => (
-        <div key={i} className="menu">
-          {<Link to={value.url}>{value.nombre}</Link>}
-          <div id="buscador">
-            <form onSubmit={(e) => e.preventDefault()}>
-              <input
-                type="search"
-                onChange={(e) => setEntrada(e.target.value)}
-              />
-              <input type="submit" />
-            </form>
-          </div>
-        </div>
+        <Link to={value.url} key={i}>
+          {value.nombre}
+        </Link>
       ))}
+      {buscador}
+      <Link to="/series">Series</Link>
     </div>
   );
 }
